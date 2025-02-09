4e4b5cd68fa322d7fd91644262efa7880a3da583
Anil2296 <anilkumarnagina007@gmail.com>
2025-02-09 23:03:02 +0530

    new

---
+++ src/test/java/PR_test/SampleTest.java
@@ -0,0 +1,40 @@
+package PR_test;
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+
+public class SampleTest {
+
+    // This will be run before the tests in this class
+    @BeforeClass
+    public void setUp() {
+        System.out.println("Setting up the test environment...");
+    }
+
+    // Sample test case 1
+    @Test
+    public void testAddition() {
+        int sum = 2 + 3;
+        Assert.assertEquals(sum, 5, "Addition test failed");
+        System.out.println("Addition test passed");
+    }
+
+    // Sample test case 2
+    @Test
+    public void testString() {
+        String message = "Hello, World!";
+        Assert.assertEquals(message, "Hello, World!", "String test failed");
+        System.out.println("String test passed");
+    }
+
+    // This will be run after the tests in this class
+    @AfterClass
+    public void tearDown() {
+        System.out.println("Tearing down the test environment...");
+    }
+}
+
